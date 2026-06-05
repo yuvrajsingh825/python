c7ca31f4bec1cf6f628d860b24623da9386ab673
Yuvraj Singh Tomar <yuvrajs6400@gmail.com>
2026-06-05 11:03:38 +0530

    List

--- list.py
+++ list.py
@@ -1,13 +1,15 @@
-skill = [ "Python", "java", "C++","git"]
-print(skill)
+# A Fruit List 
+fruit = ["apple","banana", "cherry"]
+print(fruit[0])
+print(fruit[2])
 
-print(skill[0])
-skill.append("Machine Learning")
-skill.remove("git")
-
-for s in skill:
-    print(s)
-
-print(len(skill))
-
-
+# list of 5 elements
+numbers = [1,2,3,4,5]
+for number in numbers:
+    print(number)
+    
+print(len(numbers))
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
+print(sorted(numbers))
