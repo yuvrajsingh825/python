f20d9cce93bc84b302d73b9407f78dfd6b736239
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-06-05 22:10:35 +0530

    add

---
+++ set.py
@@ -0,0 +1,9 @@
+fruit={"Mango","apple","tomato","graps","puppya"}
+print(fruit)
+
+numbers={1,2,1,2,3,4,5}
+unique_numbers = set(numbers)
+print(unique_numbers)
+print(4 in numbers)
+
+student={"rahul",}
