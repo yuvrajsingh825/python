1163799660bb30aad3b48f14f70b3097d503c85c
Yuvraj Singh Tomar <yuvrajs6400@gmail.com>
2026-06-05 11:07:33 +0530

    Student marks mannager

---
+++ marksmannager.py
@@ -0,0 +1,6 @@
+marks = [78, 85, 92, 67, 88]
+print("Marks:",marks)
+print("Highest Marks :", max(marks))
+print("Lowest Marks :", min(marks))
+print("Total marks :", sum(marks))
+print("Average marks :", sum(marks)/len(marks))
