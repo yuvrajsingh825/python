b7422da9a60c5d78e4fb67f51a3d3fdedb4a245c
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-06-05 12:05:16 +0530

    tuple.py

---
+++ tuple.py
@@ -0,0 +1,27 @@
+# Tuple is immutable but list is mutable 
+fruit=("Mango","orange","kela","tarbuz")
+print(fruit)
+print(fruit[0])
+print(fruit[3])
+
+#tuple of 5 numbers
+numbers=(1,2,3,5,5,)
+for number in numbers:
+
+    print(number)
+print(len(numbers))
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
+
+#Count how many times a value appears.
+numbers1 = [1, 2, 3, 2, 4, 2, 5]
+
+value = int(input("Enter value to count: "))
+count = 0
+
+for num in numbers1:
+    if num == value:
+        count += 1
+
+print("Count =", count)
