9cccc727732c43fbf9805a6999d6b6647fd303e0
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-06-10 12:43:41 +0530

    lambda function

---
+++ lambda.py
@@ -0,0 +1,14 @@
+#Create a lambda to square a number.
+square = lambda num :num*num
+print(square(10))
+
+#Create a lambda to cube a number.
+cube = lambda num :num*num*num
+print(cube(10))
+
+#Create a lambda to add two numbers.
+sum = lambda a,b:a+b
+print(sum(10,25))
+
+
+
