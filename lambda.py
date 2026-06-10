6345669beb7177f580c2ff35af9a3e2dbb394cec
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-06-10 17:09:49 +0530

    updated

--- lambda.py
+++ lambda.py
@@ -10,5 +10,26 @@ print(cube(10))
 sum = lambda a,b:a+b
 print(sum(10,25))
 
+#Create a lambda to multiply two numbers.
+mul = lambda a,b:a*b
+print(mul(10,25))
 
+#Create a lambda that returns the larger of two numbers.
+larger = lambda y,z: y if y > z else z
+print(larger(50,25))
+
+
+add = lambda a, b: a + b
+subtract = lambda a, b: a - b
+multiply = lambda a, b: a * b
+divide = lambda a, b: a / b
+
+
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+value = add(a, b)
+print("Addition:",value)
+
+value = subtract(a,b)
+print("Subtract:",value)
 
