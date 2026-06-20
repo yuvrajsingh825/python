d8e0388441f34d29786a534ab907bceab131af51
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-06-20 20:06:52 +0530

    adD

---
+++ async1.py
@@ -0,0 +1,22 @@
+import asyncio
+
+async def task1():
+
+    await asyncio.sleep(2)
+
+    print("Task 1")
+
+async def task2():
+
+    await asyncio.sleep(2)
+
+    print("Task 2")
+
+async def main():
+
+    await asyncio.gather(
+        task1(),
+        task2()
+    )
+
+asyncio.run(main())
